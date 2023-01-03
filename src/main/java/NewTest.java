b70ff2d108f043a9d43b043e66d080aa08e91243
vladislavkadyrov <vlad-kad00@mail.ru>
2023-01-03 19:06:51 +0500

    added sout

--- src/main/java/NewTest.java
+++ src/main/java/NewTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class NewTest {
     @Test
+    System.out.println("This is the best test");
     void  newTest() {
         Assertions.assertTrue(3>2);
     }
